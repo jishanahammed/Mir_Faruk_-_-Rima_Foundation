cbf4f97bacb8ba4ee4d485cf33cfd4142c9df759
Jishan Ahammed <jishanahammed.bubt@gmail.com>
2026-08-13 10:58:15 +0600

    ready

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -2,6 +2,7 @@ import { BankInfo } from "@/components/public/home/bank-info";
 import { SiteFooter } from "@/components/public/layout/site-footer";
 import { SiteHeader } from "@/components/public/layout/site-header";
 import { LocaleProvider } from "@/components/public/providers/locale-provider";
+import FeedbackPage from "./feedback/page";
 
 export default function PublicLayout({ children }) {
   return (
@@ -9,6 +10,7 @@ export default function PublicLayout({ children }) {
       <div className="relative flex min-h-screen flex-col overflow-hidden">
         <SiteHeader />
         <main className="flex-1">{children}</main>
+        <FeedbackPage />
         <BankInfo />
         <SiteFooter />
       </div>
